c6de52a084f0090badcd08226c1b2fd5eff6d2d8
Eiichi Takazawa <eiichi@Eiichi-MBA.local>
2019-07-15 19:54:21 +0900

    自動対戦環境大体完成

--- game/auto_game_player.cpp
+++ game/auto_game_player.cpp
@@ -1,9 +1,12 @@
 #include "game_manager.hpp"
 #include <iostream>
+#include <filesystem>
+#include <boost/format.hpp>
 
 using namespace std;
+namespace fs = std::filesystem;
 
-void battle(vector<string>& commands) {
+void battle(vector<string>& commands, string log_root_path) {
   // プロセス・pstream
   vector<bp::child> ai_processes(NUM_PLAYERS);
   vector<bp::ipstream> from_ai_pstreams(NUM_PLAYERS);
@@ -25,32 +28,73 @@ void battle(vector<string>& commands) {
   }
   
   GameManager gm;
-  // 初期状態をログへ出力
-  // gm.outputState();
+  // 初期状態をログへ出力（@todo いい感じにログ出力するクラスがあると良い）
+  const string log_dir_path = log_root_path;
+  fs::create_directories(log_dir_path);
+  
+  vector<ofstream> ofs(NUM_PLAYERS);
+  ofstream result(log_dir_path + "result.txt");
+  // ファイル名は各プレイヤーID
+  for (int i = 0; i < NUM_PLAYERS; i++) {
+    ofs[i] = move(ofstream(log_dir_path + to_string(i) + ".txt"));
+  }
 
   // 対戦シミュレーション開始
-  while(gm.getGameState() != GameManager::DOING) {
+  while(gm.getGameState() == GameManager::DOING) {
+    // 現在の状態を出力
+    for (int i = 0; i < NUM_PLAYERS; i++) {
+      ofs[i] << "[turn" << gm.turn_ << "]" << endl; 
+      gm.outputState(ofs[i], i);
+    }
+
     gm.communicateAndSimulateAllPlayerTurn(p_froms, p_tos);
-    gm.toNextTurn();
-    // gm.outputState();
+    // 直前にした行動を出力
+    for (int i = 0; i < NUM_PLAYERS; i++) gm.outputLastAction(ofs[i], i);
+
+    gm.toNextTurn(); // 注意：next_colors等が更新されてしまう
+  }
+  // 最終状態を出力
+  for (int i = 0; i < NUM_PLAYERS; i++) {
+    ofs[i] << "[endturn]" << endl; 
+    gm.outputState(ofs[i], i);
+  }
+  
+  // 結果と合わせてコマンドも出力
+  result << "win: " << gm.getGameState() << endl;
+  for (int i = 0; i < NUM_PLAYERS; i++) {
+    result << commands[i] << endl;
   }
 
   // プロセスを落とす
   for (int pid = 0; pid < NUM_PLAYERS; pid++) {
     ai_processes[pid].terminate();
+    ofs[pid].close();
   }
+  result.close();
 }
 
 // auto_game.out "使用AIを起動するコマンド" "対戦回数" "ログ出力保存先"
-int main(void) {
+int main(int argc, char* argv[]) {
+  if (argc < 4) {
+    cerr << "usage: auto_game.out log_root_path command0 command1 ";
+    cerr << "[num_battle<=999999]" << endl;
+  }
+
   // コマンドライン引数から受け取る
   vector<string> commands(NUM_PLAYERS);
-  int num_battles;
-  string log_path;
-
+  string log_root_path = argv[1]; // "../log/test/";
+  commands[0] = argv[2]; //"../ai/random/a.out";
+  commands[1] = argv[3]; // "../ai/random/a.out";
+  int num_battles = 100;
+  if (argc >= 5) {
+    num_battles = stoi(argv[4]);
+  }
+  
   // 対戦
   for (int battle_cnt = 0; battle_cnt < num_battles; battle_cnt++) {
-    battle(commands);
+    string battle_cnt_str = (boost::format("%06d") % battle_cnt).str();
+    string battle_path = log_root_path + battle_cnt_str + "/";
+    battle(commands, battle_path);
   }
 
   return 0;
